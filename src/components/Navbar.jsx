0d9b008b93deff17d7be332a2ca0a12d7d887310
mhrjnrahul <maharjanrahul58@gmail.com>
2025-04-02 17:40:10 +0545

    imported react-router and react-icons and edited navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="bg-zinc-400 text-black p-6">
+        <div className="container mx-auto flex justify-between items-center">
+            
+            <div className="text-2xl font-bold flex justify-between gap-2 items-center"> 
+                <div>
+                    Logo
+                </div>
+                <a href="/">
+                    My Shop
+                </a>
+            </div>
 
-export default Navbar
+            <div className="hidden md:flex space-x-6">
+                <a href="/" className="hover:text-gray-400">Home</a>
+                <a href="/shop" className="hover:text-gray-400">Shop</a>
+                <a href="/about" className="hover:text-gray-400">About</a>
+                <a href="/contact" className="hover:text-gray-400">Contact</a>
+                <a href="/cart" className="flex items-center hover:text-gray-400"><FaShoppingCart /></a>
+                <a href="/account" className="mr-1 flex items-center"><FaUser /></a>
+            </div>
+
+
+        </div>
+    </nav>
+  );
+};
+
+export default Navbar;
