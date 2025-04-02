ac47ecd469bd46ff97dafda14ad90cf9a254db5f
mhrjnrahul <maharjanrahul58@gmail.com>
2025-04-02 17:09:17 +0545

    fixed naming error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 
-const Navbar = () => {
+const App = () => {
   return (
     <div>
       <Navbar />
@@ -9,4 +9,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default App
