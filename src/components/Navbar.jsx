eb1b8fccc11f794ed6b0672dabe42fc18f050781
mhrjnrahul <maharjanrahul58@gmail.com>
2025-04-03 22:54:00 +0545

    completed mobile view menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import { FaShoppingCart, FaUser, FaBars, FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState("false");
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <nav className="bg-zinc-400 text-black p-6">
-      <div className="container mx-auto flex justify-around items-center">
+      <div className="container mx-auto flex justify-between items-center">
         <div className="text-2xl font-bold flex justify-between gap-2 items-center">
           <a href="/">My Shop</a>
         </div>
@@ -34,8 +34,49 @@ const Navbar = () => {
         </div>
 
         <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
-            {isOpen ? <FaTimes size={24}/> : <FaBars size={24}/>}
+          {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
         </button>
+
+        {isOpen && (
+          <div className="md:hidden bg-gray-700 absolute top-14 left-0 w-full py-4">
+            <a href="/" className="block py-2 px-4 hover:bg-gray-600">Home</a>
+            <a
+              href="/shop"
+              className="block py-2 px-4 hover:bg-gray-600"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              Shop
+            </a>
+            <a
+              href="/about"
+              className="block py-2 px-4 hover:bg-gray-600"
+              onClick={() => setIsOpen(false)}
+            >
+              About
+            </a>
+            <a
+              href="/contact"
+              className="block py-2 px-4 hover:bg-gray-600"
+              onClick={() => setIsOpen(false)}
+            >
+              Contact
+            </a>
+            <a
+              href="/cart"
+              className="block py-2 px-4 hover:bg-gray-600 flex items-center"
+              onClick={() => setIsOpen(false)}
+            >
+              <FaShoppingCart className="mr-2"/>
+              Cart
+            </a>
+            <a
+              href="/account"
+              className="block py-2 px-4 hover:bg-gray-600 flex items-center">
+                <FaUser className="mr-2"/>
+              Account
+            </a>
+          </div>
+        )}
       </div>
     </nav>
   );
