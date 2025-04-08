82f3a2e2c8c272b1d362be53ba6f2edee5d80faf
mhrjnrahul <maharjanrahul58@gmail.com>
2025-04-08 13:53:47 +0545

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
 
+import App from './App.jsx'
 import Home from './pages/Home.jsx'
 import Products from './pages/Products.jsx'
 import Categories from './pages/Categories.jsx'
