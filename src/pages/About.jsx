f9a65a78eac684b9d34a75ffebc463bf1844914e
mhrjnrahul <maharjanrahul58@gmail.com>
2025-04-09 01:36:47 +0545

    completed about me page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Footer from '../components/Footer'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className='font-mono grid grid-cols-1 max-w-4xl justify-center items-center mx-auto px-30'>
+        <div>
+        <h1 className='text-left mb-5'>ABOUT</h1>
+        <p className='text-sm'>Shop Style is a factory brand, 
+          produced by the world's best maker of fleece fabric 
+          and apparel, Roopa Knitting Mills.</p>
+        <img 
+        src='/Headphones.png'
+        alt='about'
+        className='object-cover h-full w-full mb-25'/>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
   )
 }
 
