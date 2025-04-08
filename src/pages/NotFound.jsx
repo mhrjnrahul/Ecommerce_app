92ccd74cbc8ed5edde50d36f33caf28572e37ec9
mhrjnrahul <maharjanrahul58@gmail.com>
2025-04-09 01:46:18 +0545

    edited notfound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='w-screen h-screen inset-0 fixed overflow-hidden flex justify-center items-center'>
+      <div className='text-center'>
+        <h1 className='text-5xl font-bold mb-4'>404</h1>
+        <p className='text-xl'>Page Not Found</p>
+        <p className='text-sm mt-2'>The page you are looking for does not exist.</p>
+      </div>
+    </div>
   )
 }
 
