54574b8c90827f7a1c157663b538deeb57667758
mhrjnrahul <maharjanrahul58@gmail.com>
2025-04-08 18:24:49 +0545

    API setup

---
+++ src/api/product.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API_URL = "https://fakestoreapi.com/products"; 
+
+const getProducts = async () => {
+    try {
+        const response = await axios.get(API_URL);
+        return response.data;
+    } catch (error) {
+        console.log("Error fetching products:", error);
+        throw error;
+    }
+}
+
+export default getProducts
