34acdd368dabb7daf9df9a722c453a3c63fa4138
mhrjnrahul <maharjanrahul58@gmail.com>
2025-04-09 11:46:14 +0545

    account section

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,27 +1,95 @@
-import React from 'react'
-import Button from '../components/Button'
-import { ShoppingCart, LogIn, UserPlus, Heart, CreditCard } from 'lucide-react';
+import React, { useState } from "react";
+import Button from "../components/Button";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Account = () => {
-  return (
-    <div className='w-screen h-screen overflow-hidden flex justify-center items-center bg-[#F5F5F5]'>
-        <div className='flex flex-col gap-5 h-full w-full max-w-md items-center justify-center'>
-            <h2>Account</h2>
-            <form>
-                <div className='flex flex-col gap-2'>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id='email' placeholder='Enter your email' className='border border-gray-300 p-2 rounded-md'/>
-                </div>
-                <div className='flex flex-col gap-2'>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id='password' placeholder='Enter your password' className='border border-gray-300 p-2 rounded-md'/>
-                </div>
-                <Button variant='outline'>Signup</Button>
-            </form>
-            <p>Already have an account? Login</p>
-        </div>
-    </div>
-  )
-}
+  const [hasAccount, setHasAccount] = useState(true);
 
-export default Account
+  return (
+    <>
+      <Navbar />
+
+      <div className="w-full h-auto py-10 overflow-hidden flex justify-center items-center bg-[#F5F5F5] font-mono">
+        <div className="flex flex-col gap-4 h-full w-[700px] items-center justify-center">
+          <h2 className="">{hasAccount ? "LOGIN" : "CREATE ACCOUNT"}</h2>
+          <form className="w-full md:border h-auto px-5 py-6 rounded-md">
+            <div className="flex flex-col gap-3">
+              {hasAccount ? (
+                <>
+                  <input
+                    type="email"
+                    id="email"
+                    placeholder="Email"
+                    className="border border-gray-300 p-2 rounded-md"
+                  />
+                  <input
+                    type="password"
+                    id="password"
+                    placeholder="Password"
+                    className="border border-gray-300 p-2 rounded-md"
+                  />
+                </>
+              ) : (
+                <>
+                  <input
+                    type="text"
+                    id="name"
+                    placeholder="Name"
+                    className="border border-gray-300 p-2 rounded-md"
+                  />
+                  <input
+                    type="email"
+                    id="email"
+                    placeholder="Email"
+                    className="border border-gray-300 p-2 rounded-md"
+                  />
+                  <input
+                    type="password"
+                    id="password"
+                    placeholder="Password"
+                    className="border border-gray-300 p-2 rounded-md"
+                  />
+                </>
+              )}
+            </div>
+
+            <Button
+              variant="outline"
+              className="mt-5 w-25 rounded text-white bg-black mx-auto"
+            >
+              {hasAccount ? "Login" : "Signup"}
+            </Button>
+            <p className="text-center mt-3">
+              {hasAccount ? (
+                <>
+                  Don't have an account?{" "}
+                  <span
+                    className="text-blue-500 cursor-pointer"
+                    onClick={() => setHasAccount(false)}
+                  >
+                    Signup
+                  </span>
+                </>
+              ) : (
+                <>
+                  Already have an account?{" "}
+                  <span
+                    className="text-blue-500 cursor-pointer"
+                    onClick={() => setHasAccount(true)}
+                  >
+                    Login
+                  </span>
+                </>
+              )}
+            </p>
+          </form>
+        </div>
+      </div>
+
+      <Footer />
+    </>
+  );
+};
+
+export default Account;
